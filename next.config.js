a8e533305f1632909c437f3f290aa7031c7e391f
nahyun <nhkim@whatap.io>
2023-07-24 20:11:49 +0900

    fix(index): 데이터 로드 시점 변경

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import ArticleItem from "@/components/ArticleItem";
-import { GetStaticProps } from "next";
+// import { GetStaticProps } from "next";
+import { useEffect, useState } from "react";
 
 type Post = {
   kid: number;
@@ -8,22 +9,44 @@ type Post = {
   content: string;
   url: string;
 };
-export const getStaticProps: GetStaticProps<{ data: Array<Post> }> = async () => {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/news/list?page=1&size=10&keyword=k8s`);
-    if (res.status !== 200) {
-      console.error("Failed to fetch data!");
-      return { props: { data: [] } };
-    }
-    const result: { data: Array<Post> } = await res.json();
-    return { props: result };
-  } catch {
-    console.error("Failed to fetch data!");
-    return { props: { data: [] } };
-  }
-};
+// export const getStaticProps: GetStaticProps<{
+//   data: Array<Post>;
+// }> = async () => {
+//   try {
+//     const res = await fetch(
+//       `${process.env.NEXT_PUBLIC_API_URL}/v1/news/list?page=1&size=10&keyword=k8s`
+//     );
+//     if (res.status !== 200) {
+//       console.error("Failed to fetch data!");
+//       return { props: { data: [] } };
+//     }
+//     const result: { data: Array<Post> } = await res.json();
+//     return { props: result };
+//   } catch {
+//     console.error("Failed to fetch data!");
+//     return { props: { data: [] } };
+//   }
+// };
 
-export default function Article({ data }: { data: Array<Post> }) {
+export default function Article() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(`/v1/news/list?page=1&size=10&keyword=k8s`);
+        if (res.status !== 200) {
+          console.error("Failed to fetch data!");
+        }
+        const result: { data: Array<Post> } = await res.json();
+        if (!Array.isArray(result?.data)) throw new Error("data is not array");
+        setData(result?.data);
+      } catch {
+        console.error("Failed to fetch data!");
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div>
       {data.map((post) => {
